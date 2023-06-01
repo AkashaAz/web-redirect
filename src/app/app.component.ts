43d56c3fbd7d79f01899e3cdfb312ddbe41b98d9
Thanapon <thanapon.makmesup@gmail.com>
2023-06-01 17:33:22 +0700

    fix: update code portal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,45 +1,34 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'web-redirect';
+  title = "web-redirect";
   ngOnInit(): void {
     (window as any).handleCommunication = this.handleCommunication.bind(this);
   }
   redirect() {
-    window.location.replace('https://auto-schema.web.app/callback');
+    window.location.replace("https://auto-schema.web.app/callback");
   }
   async handleCommunication(data: any) {
-    console.log(data.headers.headers);
-    for (const [key, value] of data.headers.headers) {
-      console.log(key);
-      console.log(value);
-    }
+    //add header
+    data.headers.headers.set("cache-Control", ["no-cache"]);
+    data.headers.normalizedNames.set("cache-Control", "Cache-Control");
+    // for (const [key, value] of data.headers.headers) {
+    //   console.log(key);
+    //   console.log(value);
+    // }
+    // check header
 
-    let response = {
-      url: 'https://restcountries.com/v2/name/Afghanistan',
-      headers: [
-        {
-          example: 'token',
-        },
-        {
-          example: 'token',
-        },
-        {
-          example: 'token',
-        },
-      ],
-      body: {
-        name: 'josh',
-      },
-      responseType: 'json',
-      method: 'get',
-    };
-    console.log(response);
-    return response;
+    // change url
+    data.url = "https://restcountries.com/v2/name/Afghanistan";
+    data.urlWithParams = "https://restcountries.com/v2/name/Afghanistan";
+
+    console.log(data.headers.headers);
+    console.log(data);
+    return data;
   }
 }
