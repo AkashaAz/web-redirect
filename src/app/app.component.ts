0bc263b1b7a8252b563608e25bf5bb76ea409f1c
Thanapon <thanapon.makmesup@gmail.com>
2023-06-22 18:20:08 +0700

    fix: api get jwk

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import { MiddlewareConfigService } from "./services/common/configMiddleware";
 import { Ga4Service } from "./services/common/config-GA4.service";
+import { ApiService } from "./services/api";
 
 @Component({
   selector: "app-root",
@@ -9,8 +10,9 @@ import { Ga4Service } from "./services/common/config-GA4.service";
 })
 export class AppComponent {
   title = "web-redirect";
-  constructor(private middlewareConfigService: MiddlewareConfigService, private ga4Service: Ga4Service) {}
+  constructor(private middlewareConfigService: MiddlewareConfigService, private ga4Service: Ga4Service, private apiService: ApiService) {}
   ngOnInit(): void {
+    this.getJwks();
     console.log(this.ga4Service.checkEnvConfig("insurance"));
     (window as any).checkConfigMiddleware = this.checkConfigMiddleware.bind(this);
     (window as any).handleCommunication = this.handleCommunication.bind(this);
@@ -36,5 +38,10 @@ export class AppComponent {
     console.log(data.headers.headers);
     console.log(data);
     return data;
+  }
+  private getJwks() {
+    this.apiService.getJwks().subscribe(async (res: any) => {
+      console.log(res);
+    });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { TokenInterceptor } from './http.interceptor';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { TokenInterceptor } from "./http.interceptor";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [
     {
       provide: HTTP_INTERCEPTORS,
